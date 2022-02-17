8d96e56713c0dd495bd1738e558c7c2788082c08
Ciel/howard <howardlight+github@protonmail.com>
2022-02-17 22:34:01 +0200

    Added popup menu to Cart Icon in TopBar

--- pages/TopBar.tsx
+++ pages/TopBar.tsx
@@ -23,6 +23,48 @@ import ShoppingCart from "@mui/icons-material/ShoppingCart";
 //TODO: Implement Cart System
 export default function TopBar() {
 
+    // CART BUTTON LOGIC
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event: Event | React.SyntheticEvent) => {
+        if (
+        anchorRef.current &&
+        anchorRef.current.contains(event.target as HTMLElement)
+        ) {
+        return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event: React.KeyboardEvent) {
+        if (event.key === 'Tab') {
+        event.preventDefault();
+        setOpen(false);
+        } else if (event.key === 'Escape') {
+        setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+        anchorRef.current!.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
+
+
+
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" style={{backgroundColor: "#F3D9DC", color: "#C78283"}}>
@@ -44,6 +86,39 @@ export default function TopBar() {
                         </Badge>
                     </IconButton>
                     <Popper
+                        open={open}
+                        anchorEl={anchorRef.current}
+                        role={undefined}
+                        placement="bottom-start"
+                        transition
+                        disablePortal
+                        style={{zIndex:"1"}}
+                    >
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{
+                                    transformOrigin:
+                                        placement === 'bottom-start' ? 'left top' : 'left bottom',
+                                }}
+                            >
+                                <Paper>
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList
+                                            autoFocusItem={open}
+                                            id="composition-menu"
+                                            aria-labelledby="composition-button"
+                                            onKeyDown={handleListKeyDown}
+                                        >
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
                 </Toolbar>
             </AppBar>
       </Box>
