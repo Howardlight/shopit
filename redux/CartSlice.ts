f1212689b77820067ec5c2f6df47d4969d846383
Ciel/howard <howardlight+github@protonmail.com>
2022-02-18 11:24:53 +0200

    Removed ProductWithQuantity from CartSlice Imports

--- redux/CartSlice.ts
+++ redux/CartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Product, ProductWithQuantity } from "../types";
+import { Product } from "../types";
 
 
 interface cartState {
