a4548a1d61626d60f2068e1d80cac6e9cfb4a2bc
Ciel/howard <howardlight+github@protonmail.com>
2022-02-18 12:39:58 +0200

    Revamped [id] output

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -3,29 +3,43 @@ import { Product } from "../../types";
 import Image from "next/image";
 import axios from "axios";
 
+import { Grid, Typography, } from "@mui/material";
+import { Category, Rating } from "../../types";
+
+//Components
+import TopBar from "../TopBar";
+import { GetServerSideProps } from "next";
+
+
+
 function ProductPage({product}: {product: Product}) {
     return(
-        <ul>
-            <li>{product.title}</li>
-            <li>{product.description}</li>
-            <li>{product.id}</li>
-            <li>{product.price}</li>
-        </ul>        
+        <Grid>
+
+            <TopBar />
+
+
+            <Grid style={{display: "flex", justifyContent: "center"}}>
+                <Image 
+                src={product.image} 
+                alt={product.title} 
+                loader={imageLoader} 
+                unoptimized 
+                height={"250"} 
+                width={"250"} 
+                />
+                <Grid style={{padding: "1rem"}}>
+                    <Typography variant="h5">{product.title}</Typography>
+                    <Typography variant="h6" style={{display: "flex", justifyContent: "flex-start", padding: "1rem"}}>{product.price}$</Typography>
+                </Grid>
+
+
+            </Grid>
+
+
+
+        </Grid>
     );
-}
-
-
-export async function getStaticPaths() {
-
-    const {data} = await axios.get("https://fakestoreapi.com/products");
-    const table: Product[] = data;
-
-    return {
-        paths: table.map((product) => {
-            return {params: {id: String(product.id)}}
-        }),
-        fallback: false,
-    }
 }
 
 export async function getStaticProps({params}: {params: {id: string}}) {
