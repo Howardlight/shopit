47fbcdbb382abf70de63f438603dcd70faa60fb8
Ciel/howard <howardlight+github@protonmail.com>
2022-02-18 20:53:24 +0200

    Implemented Layout to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
 
 //Redux
 import store from "../redux/store";
 import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return(
-    <> 
     <Provider store={store}>
-      <Component {...pageProps} />
+    {getLayout(<Component {...pageProps} />)}
     </Provider>
-    </>
   );
 }
 
