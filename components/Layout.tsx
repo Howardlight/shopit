4c2b2f09c421013f0c1d29538032a063db7287af
Ciel/howard <howardlight+github@protonmail.com>
2022-02-19 22:21:29 +0200

    Added Footer to Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import TopBar from "./TopBar";
-
+import Footer from "./Footer";
 
 function Layout({children}: {children: React.ReactNode}) {
     return(
         <div>
             <TopBar />
             {children}
+            <Footer />
         </div>
     );
 }
