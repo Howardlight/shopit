979abde5148a94544dc9a9f2f9706d52081633ba
Ciel/howard <howardlight+github@protonmail.com>
2022-02-24 23:39:18 +0200

    Created Wishlist Slice

---
+++ redux/WishlistSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "../types";
+
+interface wishlistState {
+    content: Product[];
+}
+
+const initialState: wishlistState = {
+    content: [],
+}
+
+const wishlistSlice = createSlice ({
+    name: "wishlist",
+    initialState,
+    reducers: {
+        addToWishlist: (state, action: PayloadAction<Product>) => {
+
+            // Look for item in state
+            // const itemExists = state.content.find((item: Product) => item.id === action.payload.id);
+
+            state.content.push({ ...action.payload });
+            console.log(`addToWishlist added Item \n-  item: ${action.payload.title}`)
+        },
+        removeFromWishlist:(state, action) => {
+            // find item
+            const index = state.content.findIndex((item) => item.id === action.payload);
+
+            console.log(`removeFromWishlist removing item: ${index}`);
+
+            // remove item from state
+            state.content.splice(index, 1);
+        },
+    }
+});
+
+export const wishlistReducer = wishlistSlice.reducer;
+
+export const {
+    addToWishlist,
+    removeFromWishlist
+} = wishlistSlice.actions;
