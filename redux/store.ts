4fb824bf58cbb464eca7ac555a8a7857ce2e5ac5
Ciel/howard <howardlight+github@protonmail.com>
2022-02-24 23:39:30 +0200

    Added Wishlist Slice to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./CartSlice";
+import { wishlistReducer } from "./WishlistSlice";
 
 const reducer = {
     cart: cartReducer,
+    wishlist: wishlistReducer,
 };
 
 const store = configureStore({
