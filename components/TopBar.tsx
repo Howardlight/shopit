6c8f055c4d0222f917b4b2161479c20965fe9b54
Ciel/howard <howardlight+github@protonmail.com>
2022-02-25 11:41:21 +0200

    Revamped TopBar completely

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -17,55 +17,64 @@ import {
     MenuItem,
 } from "@mui/material";
 import ShoppingCart from "@mui/icons-material/ShoppingCart";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import { useAppSelector } from "../redux/hooks";
 import { useCallback } from "react";
+import { useRouter } from "next/router";
 
 
 //TODO: Implement Cart System
 export default function TopBar() {
 
+    const router = useRouter();
+
+
     // CART BUTTON LOGIC
-    const [open, setOpen] = React.useState(false);
-    const anchorRef = React.useRef<HTMLButtonElement>(null);
+    // const [open, setOpen] = React.useState(false);
+    // const anchorRef = React.useRef<HTMLButtonElement>(null);
 
-    const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-    };
+    // const handleToggle = () => {
+    //     setOpen((prevOpen) => !prevOpen);
+    // };
 
-    const handleClose = (event: Event | React.SyntheticEvent) => {
-        if (
-        anchorRef.current &&
-        anchorRef.current.contains(event.target as HTMLElement)
-        ) {
-        return;
-        }
+    // const handleClose = (event: Event | React.SyntheticEvent) => {
+    //     if (
+    //     anchorRef.current &&
+    //     anchorRef.current.contains(event.target as HTMLElement)
+    //     ) {
+    //     return;
+    //     }
 
-        setOpen(false);
-    };
+    //     setOpen(false);
+    // };
 
-    function handleListKeyDown(event: React.KeyboardEvent) {
-        if (event.key === 'Tab') {
-        event.preventDefault();
-        setOpen(false);
-        } else if (event.key === 'Escape') {
-        setOpen(false);
-        }
-    }
+    // function handleListKeyDown(event: React.KeyboardEvent) {
+    //     if (event.key === 'Tab') {
+    //     event.preventDefault();
+    //     setOpen(false);
+    //     } else if (event.key === 'Escape') {
+    //     setOpen(false);
+    //     }
+    // }
 
     // return focus to the button when we transitioned from !open -> open
-    const prevOpen = React.useRef(open);
-    React.useEffect(() => {
-        if (prevOpen.current === true && open === false) {
-        anchorRef.current!.focus();
-        }
+    // const prevOpen = React.useRef(open);
+    // React.useEffect(() => {
+    //     if (prevOpen.current === true && open === false) {
+    //     anchorRef.current!.focus();
+    //     }
 
-        prevOpen.current = open;
-    }, [open]);
+    //     prevOpen.current = open;
+    // }, [open]);
+
 
     // REACT REDUX
     const cart = useAppSelector((state) => state.cart.content);
-    const getTotalItemsCount = useCallback((): number => {
+    const wishlist = useAppSelector((state) => state.wishlist.content);
+
+
+    const getTotalCartItems = useCallback((): number => {
         let out: number = 0
         for(let i: number = 0; i < cart.length; i++) {
             out += cart[i].quantity!; 
@@ -77,10 +86,101 @@ export default function TopBar() {
         return out;
     },[cart]);
 
-    React.useEffect(() => {
-        getTotalItemsCount();
-        console.log(`useEffect | NEW RENDER`);
-    }, [cart, getTotalItemsCount])
+
+    // React.useEffect(() => {
+    //     getTotalCartItems();
+    //     console.log(`useEffect | NEW RENDER`);
+    // }, [cart, getTotalCartItems])
+
+    // ICONS
+    const ShoppingCartIcon = () => {
+        return (
+            <Badge badgeContent={getTotalCartItems()} color="error">
+                <ShoppingCart />
+            </Badge>
+        );
+    };
+    const WishlistIcon = () => {
+        return (
+            <Badge badgeContent={wishlist.length == 0 ? 0 : wishlist.length} color="error">
+                <FavoriteIcon />
+            </Badge>
+        );
+    };
+
+    const WishlistButton = () => {
+        // console.log("WhishlistButton Rerender");
+        // NOTE: This also re-renders when it doesn't need to
+        // same case as ShoppingCartButton
+
+        return (
+            <Button endIcon={<WishlistIcon />}>
+                Wishlist
+            </Button>
+        );
+    }
+    const ShoppingCartButton = () => {
+
+        // console.log("ShoppingCartButton Rerender");
+        //TODO: Button re-renders each time an item is added, Fix this\
+        // make it so only the badge re-renders
+        //NOTE: It also re-renders when a wishlist item is added
+
+        return (
+            <>
+                <Button
+                    color="inherit"
+                    // ref={anchorRef}
+                    id="composition-button"
+                    // aria-controls={open ? "composition-menu" : undefined}
+                    // aria-expanded={open ? "true" : undefined}
+                    aria-haspopup="true"
+                    onClick={() => router.push("/cart")}
+                    endIcon={<ShoppingCartIcon />}
+                >
+                    Your Cart
+                </Button>
+                {/* <Popper
+                    open={open}
+                    anchorEl={anchorRef.current}
+                    role={undefined}
+                    placement="bottom-start"
+                    transition
+                    disablePortal
+                    style={{ zIndex: "1" }}
+                >
+                    {({ TransitionProps, placement }) => (
+                        <Grow
+                            {...TransitionProps}
+                            style={{
+                                transformOrigin:
+                                    placement === 'bottom-start' ? 'left top' : 'left bottom',
+                            }}
+                        >
+                            <Paper>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                    <MenuList
+                                        autoFocusItem={open}
+                                        id="composition-menu"
+                                        aria-labelledby="composition-button"
+                                        onKeyDown={handleListKeyDown}
+                                    >
+                                        <MenuItem onClick={handleClose}>
+                                            <Link href={"/cart"}>
+                                                <a>
+                                                    Go to Cart
+                                                </a>
+                                            </Link>
+                                        </MenuItem>
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Grow>
+                    )}
+                </Popper> */}
+            </>
+        );
+    };
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -93,57 +193,8 @@ export default function TopBar() {
                             </a>
                         </Link>
                     </Typography>
-                    <IconButton 
-                        color="inherit"
-                        ref={anchorRef}
-                        id="composition-button"
-                        aria-controls={open ? "composition-menu" : undefined}
-                        aria-expanded={open ? "true" : undefined}
-                        aria-haspopup="true"
-                        onClick={handleToggle}
-                    >
-                        <Badge badgeContent={getTotalItemsCount()} color="error">                            
-                            <ShoppingCart />
-                        </Badge>
-                    </IconButton>
-                    <Popper
-                        open={open}
-                        anchorEl={anchorRef.current}
-                        role={undefined}
-                        placement="bottom-start"
-                        transition
-                        disablePortal
-                        style={{zIndex:"1"}}
-                    >
-                        {({ TransitionProps, placement }) => (
-                            <Grow
-                                {...TransitionProps}
-                                style={{
-                                    transformOrigin:
-                                        placement === 'bottom-start' ? 'left top' : 'left bottom',
-                                }}
-                            >
-                                <Paper>
-                                    <ClickAwayListener onClickAway={handleClose}>
-                                        <MenuList
-                                            autoFocusItem={open}
-                                            id="composition-menu"
-                                            aria-labelledby="composition-button"
-                                            onKeyDown={handleListKeyDown}
-                                        >
-                                            <MenuItem onClick={handleClose}>
-                                                <Link href={"/cart"}>
-                                                    <a>
-                                                        Go to Cart
-                                                    </a>
-                                                </Link>
-                                            </MenuItem>
-                                        </MenuList>
-                                    </ClickAwayListener>
-                                </Paper>
-                            </Grow>
-                        )}
-                    </Popper>
+                    <WishlistButton />
+                    <ShoppingCartButton />
                 </Toolbar>
             </AppBar>
       </Box>
