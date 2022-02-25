f7d2f849cca321c62491ac155900ef9402119e81
Ciel/howard <howardlight+github@protonmail.com>
2022-02-25 22:46:26 +0200

    Drawer is now functional, revamped Drawer

--- components/WishlistDrawer.tsx
+++ components/WishlistDrawer.tsx
@@ -1,36 +1,55 @@
 import * as React from 'react';
 import {
     Toolbar,
-    Divider,
     List,
     ListItem,
     ListItemIcon,
     ListItemText,
-    Drawer
+    Drawer,
+    Typography,
+    Box,
+    Container,
+    IconButton,
 } from "@mui/material";
 
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+import Divider from "@mui/material/Divider";
+
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+
+import Image from "next/image";
+import imageLoader from "../imageLoader";
+
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function WishlistDrawer({isDrawerOpen, setIsDrawerOpen}: {isDrawerOpen: boolean, setIsDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>}) {
-    // const { window } = props;
-    // const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    // const toggleDrawer = () => {
-    //     // setMobileOpen(!mobileOpen);
-    //     setIsDrawerOpen(!isDrawerOpen);
-    // };
+    const dispatch = useAppDispatch();
+    const wishlist = useAppSelector((state) => state.wishlist.content);
+
 
     return (
-        <div>
-            <Drawer
-                anchor={"right"}
-                open={isDrawerOpen}
-                onClose={()=> setIsDrawerOpen(false)}
-            >
-                Test
-            </Drawer>
-        </div>
+        <Drawer
+            anchor={"right"}
+            open={isDrawerOpen}
+            onClose={() => setIsDrawerOpen(false)}
+        >
+            {wishlist.map((product, index) => {
+                return (
+                    <>
+                        <Box key={index} style={{ display: "flex", justifyContent: "center", flexDirection: "row", maxWidth: "300px", padding: "3px" }}>
+                            <Image loader={imageLoader} alt={product.title} src={product.image} unoptimized height="128" width="128" />
+                            <Container>
+                                <Typography variant="subtitle1">{product.title}</Typography>
+                                <Box style={{ display: "flex", justifyContent: "space-evenly", flexDirection: "row", alignItems: "center" }}>
+                                    <Typography variant="subtitle2">{product.category}</Typography>
+                                    <IconButton><DeleteIcon /></IconButton>
+                                </Box>
+                            </Container>
+                        </Box>
+                        <Divider />
+                    </>
+                );
+            })}
+        </Drawer>
     );
 };
