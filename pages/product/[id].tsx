107c2b32646b788773834653e93d8744b09d5d84
HowardLight <howardlight@protonmail.com>
2022-05-02 20:44:07 +0300

    Added Error Prompt for Size, Implemented Size

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,8 +1,8 @@
 import imageLoader from "../../imageLoader";
-import {Product} from "../../utils/types";
+import {Product, Size} from "../../utils/types";
 import Image from "next/image";
 import axios from "axios";
-import {Box, Button, Grid, Typography,} from "@mui/material";
+import {Alert, Box, Button, Collapse, Grid, Typography,} from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -10,16 +10,21 @@ import * as React from "react";
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
 import Layout from "../../components/Layout";
 import {useAppDispatch, useAppSelector} from "../../redux/hooks";
-import {addToCart} from "../../redux/CartSlice";
 import {addToWishlist, removeFromWishlist} from "../../redux/WishlistSlice";
 import {checkIfInWishlist} from "../../utils/WishlistUtils";
 import styles from "../../styles/[id].module.css";
+import {addToCart} from "../../redux/CartSlice";
 
 function ProductPage({product}: { product: Product }) {
 
     const dispatch = useAppDispatch();
     const wishlist = useAppSelector((state) => state.wishlist.content);
     const [isWishlisted, setIsWishlisted] = useState(false);
+
+    //TODO: if size is null display error
+    //TODO: create a warning bar for when size is null
+    const [size, setSize] = useState< Size | null >(null);
+    const [displayError, setDisplayError] = useState<boolean>(false);
 
     useEffect(() => {
         // console.warn("useEffect Rerendered");
@@ -28,7 +33,6 @@ function ProductPage({product}: { product: Product }) {
         else setIsWishlisted(false);
 
     }, [isWishlisted, wishlist, product]);
-
 
     function handleWishlistButton(product: Product) {
         // console.log("HandleWishlist Button rendered");
@@ -39,10 +43,20 @@ function ProductPage({product}: { product: Product }) {
             setIsWishlisted(true);
         }
     }
+    function handleAddToCartButton(product: Product) {
 
-    //TODO: if size is null display error
-    //TODO: create a warning bar for when size is null
-    const [size, setSize] = useState<string | null>(null);
+        // If user did not select a size, Promp Error
+        if(size == null) {
+            setDisplayError(true);
+            return ;
+        }
+
+        let toBeCartedProduct: Product = product;
+        toBeCartedProduct.Size = size;
+
+        dispatch(addToCart(toBeCartedProduct));
+    }
+
 
     return (
         <Grid className={styles.mainGrid}>
@@ -63,7 +77,8 @@ function ProductPage({product}: { product: Product }) {
                         variant="contained"
                         color="primary"
                         endIcon={<ShoppingCartIcon/>}
-                        onClick={() => dispatch(addToCart(product))}
+                        // onClick={() => dispatch(addToCart(product))}
+                        onClick={() => handleAddToCartButton(product)}
                     >
                         Add To Cart
                     </Button>
@@ -76,10 +91,13 @@ function ProductPage({product}: { product: Product }) {
                         {isWishlisted ? "Remove from Wishlist" : "Add to Wishlist"}
                     </Button>
                     <ItemSizesGroup setSize={setSize}/>
+                    <Collapse in={displayError}>
+                        <Alert severity={"error"} onClose={() => setDisplayError(false)}>You must select a <strong>size</strong></Alert>
+                    </Collapse>
                 </Box>
-
                 <br/>
                 <Typography variant="body1" gutterBottom>{product.description}</Typography>
+
             </Grid>
         </Grid>
     );
@@ -89,13 +107,13 @@ function ProductPage({product}: { product: Product }) {
 //TODO: maybe export this along with ItemSizesGroup
 function ItemSizeButton(
     {size, keyId, isActive, setIsActive, setSize}:
-    { size: string, keyId: string, isActive: string | null, setIsActive: Dispatch<SetStateAction<string | null>>, setSize: Dispatch<SetStateAction<string | null>> }) {
+    { size: string, keyId: string, isActive: string | null, setIsActive: Dispatch<SetStateAction<string | null>>, setSize: Dispatch<SetStateAction<Size | null>> }) {
     return (
         <Button
             variant={isActive === keyId ? "contained" : "outlined"}
             onClick={() => {
                 setIsActive(keyId);
-                setSize(keyId);
+                setSize(keyId as Size);
             }}
         >
             {size}
@@ -103,14 +121,14 @@ function ItemSizeButton(
     )
 }
 
-function ItemSizesGroup({setSize}: { setSize: Dispatch<SetStateAction<string | null>> }) {
+function ItemSizesGroup({setSize}: { setSize: Dispatch<SetStateAction< Size | null>> }) {
 
     // used to specify which size is Active
     const [isActive, setIsActive] = useState<string | null>(null);
 
     // Sizes: S|M|L|XL
     //TODO: Global this, maybe export it
-    const sizes: Array<string> = ["S", "M", "L", "XL"];
+    const sizes: Size[] = [Size.Small, Size.Medium, Size.Large, Size.ExtraLarge];
     return (
         <React.Fragment>
             <div
