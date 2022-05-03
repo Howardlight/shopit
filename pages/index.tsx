e9017412d570a938974d6ad8e6cfc294f6ef3655
HowardLight <howardlight@protonmail.com>
2022-05-03 21:50:56 +0300

    Created men's clothing category and woman's clothing category

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,71 @@
-
 import * as React from "react";
-
-// NEXT INTERFACES
-import type {
-  GetStaticProps,
-  // NextPage,
-  // GetServerSideProps 
-} from 'next'
-
-// NEXT
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import Link from 'next/link';
-
-const axios = require('axios').default;
-import { Product } from '../utils/types';
-import { Grid } from "@mui/material";
-
-// Components
+import type {GetStaticProps,} from 'next'
+import {Category, Product} from '../utils/types';
+import {Box, Grid, Typography} from "@mui/material";
 import Layout from '../components/Layout';
 import ProductCard from "../components/ProductCard";
-
 import styles from "../styles/Home.module.css";
 
-//TODO: add category based searches
-function Home({ productTable }: { productTable: Product[] }) {
+const axios = require('axios').default;
 
-  return (
-    <Grid>
-      <Grid container columns={{xs: 4, sm: 8, md: 12, lg: 16}} className={styles.mainGrid}>
-        {productTable.map((product) => {
-          return (
-            <Grid item xs={4} sm={4} md={4} lg={4} key={product.id} style={{ padding: "25px" }}>
-              <ProductCard
-                product={product}
-                key={product.id}
-              />
+
+//TODO: add category based searches
+function Home({productTable}: { productTable: Product[] }) {
+
+
+    let mensClothing = productTable.filter(function (product) {
+            return product.category == Category.MenSClothing;
+    });
+    mensClothing.splice(4);
+
+    let womansClothing = productTable.filter(function(product) {
+        return product.category == Category.WomenSClothing;
+    });
+    womansClothing.splice(4);
+
+    return (
+        <Grid style={{margin: "40px"}}>
+
+            <Box component={"div"} style={{padding: "10px"}}>
+                <Typography variant={"h6"}>{"Men's Clothing"}</Typography>
+                <Grid container columns={{xs: 4, sm: 8, md: 12, lg: 16}}>
+                    {mensClothing.map((product) => {
+                        return(
+                            <Grid item  xs={2} sm={2} md={3} lg={3} key={product.id}>
+                                <ProductCard product={product} />
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
+
+            <Box component={"div"} style={{padding: "10px"}}>
+                <Typography variant={"h6"}>{"Woman's Clothing"}</Typography>
+                <Grid container columns={{xs: 4, sm: 8, md: 12, lg: 16}}>
+                    {womansClothing.map((product) => {
+                        return(
+                            <Grid item  xs={2} sm={2} md={3} lg={3} key={product.id}>
+                                <ProductCard product={product} />
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
+
+            <Grid container columns={{xs: 4, sm: 8, md: 12, lg: 16}} className={styles.mainGrid}>
+                {productTable.map((product, index) => {
+                    return (
+                        <Grid item xs={4} sm={4} md={4} lg={4} key={product.id} style={{padding: "25px"}}>
+                            <ProductCard
+                                product={product}
+                                key={product.id}
+                            />
+                        </Grid>
+                    );
+                })}
             </Grid>
-          );
-        })}
-      </Grid>
-    </Grid>
-  );
+        </Grid>
+    );
 }
 
 
