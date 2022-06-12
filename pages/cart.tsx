6001c3f09106cfb918b0fc9880845b3fb29ee075
HowardLight <howardlight@protonmail.com>
2022-06-12 23:04:32 +0300

    Created New Cards for cart.tsx

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,20 +1,7 @@
+import {useAppDispatch, useAppSelector} from "../redux/hooks";
+
+import {Box, ButtonGroup, Card, CardContent, CardMedia, Container, Typography, IconButton} from "@mui/material";
 import Image from "next/image";
-
-import { useAppSelector, useAppDispatch} from "../redux/hooks";
-import {
-    incrementQuantity,
-    decrementQuantity,
-    removeFromCart,
-} from "../redux/CartSlice";
-
-import {
-    IconButton,
-    ButtonGroup,
-    Container,
-    Typography,
-    Box
-} from "@mui/material";
-
 
 // Styles
 import styles from "../styles/Cart.module.css";
@@ -29,7 +16,11 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import Head from "next/head";
-import React from "react";
+import React, {Dispatch} from "react";
+import {Product} from "../utils/types";
+import {AnyAction} from "redux";
+import {decrementQuantity, incrementQuantity, removeFromCart} from "../redux/CartSlice";
+
 CartPage.getLayout = function getLayout(page: typeof CartPage) {
     return <Layout>{page}</Layout>
 }
@@ -67,30 +58,7 @@ function CartPage() {
                 ) : (
                     <>
                         {cart.map((item, index) => (
-                            <div
-                                className={styles.body}
-                                key={index}>
-                                <div
-                                    className={styles.image}
-                                >
-                                    <Image loader={imageLoader} unoptimized src={item.image} alt={item.title} height="90" width="65" />
-                                </div>
-                                <Typography variant="subtitle2">{item.title}</Typography>
-                                <Typography>$ {item.price}</Typography >
-                                <Typography >{item.quantity}</Typography >
-                                <ButtonGroup className={styles.mainButtonGroup}>
-                                    <IconButton color="primary" onClick={() => dispatch(incrementQuantity(item.id))}>
-                                        <AddIcon />
-                                    </IconButton>
-                                    <IconButton color="primary" onClick={() => dispatch(decrementQuantity(item.id))}>
-                                        <RemoveIcon />
-                                    </IconButton>
-                                    <IconButton color="error" onClick={() => dispatch(removeFromCart(item.id))}>
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </ButtonGroup>
-                                <Typography variant="subtitle2">$ {Math.round((item.quantity! * item.price) * 100) / 100}</Typography>
-                            </div>
+                            <CartCard product={item} key={index} dispatch={dispatch} />
                         ))}
                         <h2>Grand Total: $ {Math.round(getTotalPrice() * 100) / 100}</h2>
                     </>
@@ -98,6 +66,57 @@ function CartPage() {
             </Container>
         </React.Fragment>
     );
-};
+}
+
+function CartCard({product, key, dispatch}: {product: Product, key: number, dispatch: Dispatch<AnyAction> }) {
+
+    return(
+        <React.Fragment key={key}>
+            <Card sx={{height: 150, margin: "25px"}} style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+            }}>
+
+                <CardMedia
+                    component={"img"}
+
+                    style={{width: "auto", objectFit: "contain", padding: "5px"}}
+
+                    image={product.image}
+                />
+
+
+                <CardContent style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}} sx={{flexGrow: 3}}>
+
+
+                    <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                        <Typography variant={"subtitle2"}>{product.title}</Typography>
+                        <Typography>Item Price: {product.price}</Typography>
+                        <Typography>Quantity: {product.quantity}</Typography>
+                        {product.Size ? <Typography>Size: {product.Size}</Typography> : <></>}
+                    </Box>
+
+
+                    <ButtonGroup
+                        // className={styles.mainButtonGroup}
+                        style={{display: "flex", flexDirection: "column"}}
+                    >
+                        <IconButton color="primary" onClick={() => dispatch(incrementQuantity(product))}>
+                            <AddIcon />
+                        </IconButton>
+                        <IconButton color="primary" onClick={() => dispatch(decrementQuantity(product))}>
+                            <RemoveIcon />
+                        </IconButton>
+                        <IconButton color="error" onClick={() => dispatch(removeFromCart(product))}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </ButtonGroup>
+                </CardContent>
+
+            </Card>
+        </React.Fragment>
+    )
+}
 
 export default CartPage;
