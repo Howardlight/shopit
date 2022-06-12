86c52a9916a59baacf5f1f7de9b108d1d671bdf4
HowardLight <howardlight@protonmail.com>
2022-06-12 22:53:07 +0300

    Improved CartSlice.ts redux logic

--- redux/CartSlice.ts
+++ redux/CartSlice.ts
@@ -14,6 +14,8 @@ const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
+        //TODO: Use EsDocs to explain this
+        //TODO: Simplify this using functions, if possible
         addToCart: (state, action: PayloadAction<Product>) => {
 
             let itemExists = null;
@@ -22,7 +24,6 @@ const cartSlice = createSlice({
                 action.payload.category == Category.MenSClothing ||
                 action.payload.category == Category.WomenSClothing
             ) {
-                // Filters by id AND size
                 itemExists = state.content.find((item: Product) => item.id === action.payload.id && item.Size == action.payload.Size);
             } else {
                 // Filters by id ONLY
@@ -41,59 +42,97 @@ const cartSlice = createSlice({
             }
         },
         //TODO: Update this for Size Elements and categories
-        incrementQuantity: (state, action) => {
+        incrementQuantity: (state, action: PayloadAction<Product>) => {
 
-            // Look for item in state
-            const item = state.content.find((item) => item.id === action.payload);
+            let itemExists = null;
+            // Look for item is Clothing
+            if(
+                action.payload.category == Category.MenSClothing ||
+                action.payload.category == Category.WomenSClothing
+            ) {
+                // Filters by id AND size
+                itemExists = state.content.find((item: Product) => item.id === action.payload.id && item.Size == action.payload.Size && item.category === action.payload.category);
+            } else {
+                // Filters by id ONLY
+                itemExists = state.content.find((item: Product) => item.id === action.payload.id);
+            }
 
             // If item not found, throw error
-            if (!item) console.error(`incrementQuantity ITEM NOT FOUND`);
+            if (!itemExists) console.error(`incrementQuantity ITEM NOT FOUND`);
             else {
                 // if item has no quantity throw error
-                if (!item.quantity) console.error(`incrementQuantity item: ${item.title} has NO QUANTITY`);
+                if (!itemExists.quantity) console.error(`incrementQuantity item: ${itemExists.title} has NO QUANTITY`);
 
                 // Increase quantity 
                 else {
-                    item.quantity++;
+                    itemExists.quantity++;
                     // console.log(`incrementQuantity increased Quantity \n-  item: ${item.title} \n-  quantity: ${item.quantity}`)
                 }
             }
         },
-        decrementQuantity: (state, action) => {
+        decrementQuantity: (state, action: PayloadAction<Product>) => {
 
-            // Look for item in state
-            const item = state.content.find((item) => item.id === action.payload);
+            let itemExists = null;
+            let isClothing = false;
+            //Check if item is Clothing
+            if(
+                action.payload.category == Category.MenSClothing ||
+                action.payload.category == Category.WomenSClothing
+            ) {
+                itemExists = state.content.find((item: Product) => item.id === action.payload.id && item.Size == action.payload.Size && item.category === action.payload.category);
+                isClothing = true;
+            } else {
+                // Filters by id ONLY
+                itemExists = state.content.find((item: Product) => item.id === action.payload.id);
+            }
 
             // If item not found, throw error
-            if (!item) console.error(`decrementQuantity ITEM NOT FOUND`);
+            if (!itemExists) console.error(`decrementQuantity ITEM NOT FOUND`);
 
             else {
 
                 // If quantity is 1, remove it from state
-                if (item.quantity === 1) {
+                if (itemExists.quantity === 1) {
                     // console.log(`decrementQuantity item quantity was 1 -  removing item: ${item.title}`)
+                    let index: number;
 
-                    const index = state.content.findIndex((item) => item.id === action.payload);
+                    // Find Item then remove it
+                    if(isClothing) index = state.content.findIndex((item) => item.id === action.payload.id && item.Size === action.payload.Size && item.category === action.payload.category);
+                    else index = state.content.findIndex((item) => item.id === action.payload.id);
+
+
                     state.content.splice(index, 1);
-
 
                 } else {
 
                     // if quantity is 0, throw error
-                    if (!item.quantity) console.error(`decrementQuantity item: ${item.title} has NO QUANTITY`);
+                    if (!itemExists.quantity) console.error(`decrementQuantity item: ${itemExists.title} has NO QUANTITY`);
 
 
                     else {
                         // decrease quantity
-                        item.quantity--;
+                        itemExists.quantity--;
                         // console.log(`decrementQuantity decreased Quantity \n-  item: ${item} \n-  quantity: ${item.quantity}`)
                     }
                 }
             }
         },
-        removeFromCart: (state, action) => {
+        removeFromCart: (state, action: PayloadAction<Product>) => {
             // find item
-            const index = state.content.findIndex((item) => item.id === action.payload);
+            // const index = state.content.findIndex((item) => item.id === action.payload.id);
+            let index: number;
+            // let isClothing = false;
+            //Check if item is Clothing
+            if(
+                action.payload.category == Category.MenSClothing ||
+                action.payload.category == Category.WomenSClothing
+            ) {
+                index = state.content.findIndex((item: Product) => item.id === action.payload.id && item.Size == action.payload.Size && item.category === action.payload.category);
+                // isClothing = true;
+            } else {
+                // Filters by id ONLY
+                index = state.content.findIndex((item: Product) => item.id === action.payload.id);
+            }
 
             // console.log(`removeFromCart removing item: ${index}`);
 
