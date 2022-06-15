f7df073089f04c82fca2838ea7991991c5ed47cd
HowardLight <howardlight@protonmail.com>
2022-06-15 12:30:11 +0300

    Code Cleanup in CartSlice.ts

--- redux/CartSlice.ts
+++ redux/CartSlice.ts
@@ -42,23 +42,16 @@ const cartSlice = createSlice({
         //TODO: Use EsDocs to explain this
         //TODO: Simplify this using functions, if possible
         addToCart: (state, action: PayloadAction<Product>) => {
-
             let itemExists = findItem(action.payload, state);
 
             // if it exists, increase its quantity
-            if (itemExists) {
-                itemExists.quantity!++; // NOTE: ! Assertion here
-                // console.log(`addToCart increased Quantity \n-  item: ${itemExists.title} \n-  quantity: ${itemExists.quantity}`)
+            if (itemExists) itemExists.quantity!++;
 
-                // If it doesn't exist, add it to state
-            } else {
-                state.content.push({ ...action.payload, quantity: 1 });
-                // console.log(`addToCart added Item \n-  item: ${action.payload.title} \n-  quantity: ${action.payload.quantity}`)
-            }
+            // If it doesn't exist, add it to state
+            else state.content.push({ ...action.payload, quantity: 1 });
         },
         //TODO: Update this for Size Elements and categories
         incrementQuantity: (state, action: PayloadAction<Product>) => {
-
             let itemExists = findItem(action.payload, state);
 
             // If item not found, throw error
@@ -66,51 +59,33 @@ const cartSlice = createSlice({
             else {
                 // if item has no quantity throw error
                 if (!itemExists.quantity) console.error(`incrementQuantity item: ${itemExists.title} has NO QUANTITY`);
+                else itemExists.quantity++;
 
-                // Increase quantity 
-                else {
-                    itemExists.quantity++;
-                    // console.log(`incrementQuantity increased Quantity \n-  item: ${item.title} \n-  quantity: ${item.quantity}`)
-                }
             }
         },
         decrementQuantity: (state, action: PayloadAction<Product>) => {
-
             let itemExists = findItem(action.payload, state);
 
             // If item not found, throw error
             if (!itemExists) console.error(`decrementQuantity ITEM NOT FOUND`);
-
             else {
 
                 // If quantity is 1, remove it from state
                 if (itemExists.quantity === 1) {
-                    // console.log(`decrementQuantity item quantity was 1 -  removing item: ${item.title}`)
                     let index: number = findIndexOfItem(action.payload, state);
-
                     state.content.splice(index, 1);
 
                 } else {
-
                     // if quantity is 0, throw error
                     if (!itemExists.quantity) console.error(`decrementQuantity item: ${itemExists.title} has NO QUANTITY`);
+                    else itemExists.quantity--;
 
-
-                    else {
-                        // decrease quantity
-                        itemExists.quantity--;
-                        // console.log(`decrementQuantity decreased Quantity \n-  item: ${item} \n-  quantity: ${item.quantity}`)
-                    }
                 }
             }
         },
         removeFromCart: (state, action: PayloadAction<Product>) => {
-            // find item
             let index: number = findIndexOfItem(action.payload, state);
 
-            // console.log(`removeFromCart removing item: ${index}`);
-
-            // remove item from state
             state.content.splice(index, 1);
         },
     },
